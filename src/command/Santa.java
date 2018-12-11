96d9681866d039b0300d135db8bde9f47dda3b75
Omar <omar.veyron@gmail.com>
2018-12-11 11:49:16 +0800

    Add sounds when Santa appears.

--- src/command/Santa.java
+++ src/command/Santa.java
@@ -5,15 +5,20 @@ import javafx.scene.image.ImageView;
 
 public class Santa {
     private static Boolean status = false;
+    private static SantaAudio santaAudio = new SantaAudio();
 
     public void on() {
         status = true;
         System.out.println("The Santa Claus is displayed on the screen.");
+
+        santaAudio.play();
     }
 
     public void off() {
         status = false;
         System.out.println("The Santa Claus is hidden from the screen.");
+
+        santaAudio.stop();
     }
 
     public void on(ImageView imageView) {
@@ -21,6 +26,8 @@ public class Santa {
         System.out.println("The Santa Claus is displayed on the screen.");
 
         imageView.setVisible(true);
+
+        santaAudio.play();
     }
 
     public void off(ImageView imageView) {
@@ -28,6 +35,8 @@ public class Santa {
         System.out.println("The Santa Claus is hidden from the screen.");
 
         imageView.setVisible(false);
+
+        santaAudio.stop();
     }
 
     public Boolean getStatus() {
